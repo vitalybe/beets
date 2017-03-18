5db61fab1bd606ae5429ed448375f014d6e89a6c
Vitaly Belman <vitaly@telefonica.com>
2017-03-18 15:14:42 +0200

    parametrize song power calculation + post limit by low rating

--- beetsplug/radio_stream/playlist_generator.py
+++ beetsplug/radio_stream/playlist_generator.py
@@ -41,20 +41,26 @@ def rule_play_rating(track, power=10):
         return 0
 
 
-def rule_play_last_time(track, power=15):
-    MAX_PLAYED_LAST_DAYS = 300
+def rule_play_last_time(track, max_last_played_days=300, power=15):
     if "lastplayed" in track:
         last_played = datetime.fromtimestamp(track.lastplayed)
         days_passed = (datetime.now()-last_played).days
     else:
         days_passed = sys.maxint
 
-    days_passed = min(MAX_PLAYED_LAST_DAYS, days_passed)
+    days_passed = min(max_last_played_days, days_passed)
 
-    return float(days_passed) / MAX_PLAYED_LAST_DAYS * power
+    return min(max_last_played_days, float(days_passed)) / max_last_played_days * power
 
 
-def rule_not_played_too_early(track, power=-1000):
+def rule_not_played_too_early(track,
+                              star_1_min_days=84,
+                              star_2_min_days=42,
+                              star_3_min_days=21,
+                              star_4_min_days=16,
+                              star_5_min_days=14,
+                              unrated_min_days=3,
+                              power=-1000):
     if "lastplayed" in track:
         last_played = datetime.fromtimestamp(track.lastplayed)
         days_passed = (datetime.now()-last_played).days
@@ -63,13 +69,13 @@ def rule_not_played_too_early(track, power=-1000):
 
     min_days_for_rating = {
         # Rating: Min days
-        20: 84,
-        40: 42,
-        60: 21,
-        80: 16,
-        100: 14,
+        20: star_1_min_days,
+        40: star_2_min_days,
+        60: star_3_min_days,
+        80: star_4_min_days,
+        100: star_5_min_days,
         # Unrated (new) songs
-        0: 3
+        0: unrated_min_days
     }
 
     rating = track.get("rating", 0)
@@ -83,10 +89,9 @@ def rule_not_played_too_early(track, power=-1000):
         return 0
 
 
-def rule_play_count(track, power=-15):
-    MAX_COUNT = 100
+def rule_play_count(track, max_playcount_affect=100, power=-15):
     playcount = track.get("playcount", 0)
-    return min(MAX_COUNT, float(playcount)) / MAX_COUNT * power
+    return min(max_playcount_affect, float(playcount)) / max_playcount_affect * power
 
 
 def rule_new_song(track, power=30):
@@ -96,18 +101,33 @@ def rule_new_song(track, power=30):
         return 0
 
 
-def post_rule_limit_artists(sorted_tracks):
+def post_rule_limit_artists(sorted_tracks, power=2):
     """Reduce the score of tracks with artists that
     appeared before"""
     artists = {}
     for track in sorted_tracks:
         if track.artist in artists:
             artists[track.artist] += 1
-            score = -(artists[track.artist]**2)
+            score = -(artists[track.artist]*power)
             track.scores["post_rule_limit_artists"] = score
-            log.debug(u"Post score to track {0}: {1}".format(track, score))
+            log.debug(u"Post score 'post_rule_limit_artists' to track {0}: {1}".format(track, score))
         else:
             artists[track.artist] = 0
+
+
+def post_rule_limit_low_rating(sorted_tracks, low_rating=40, power=2):
+    """Reduce the score of tracks with low rating"""
+    rule_name = "post_rule_limit_by_low_rating"
+
+    rating_count = 0
+
+    for track in sorted_tracks:
+        rating = track.get("rating", 0)
+        if low_rating >= rating > 0:
+            rating_count += 1
+            score = -(rating_count*power)
+            track.scores[rule_name] = score
+            log.debug(u"Post score '{0}', to track {1}: {2}".format(rule_name, track, score))
 
 
 def generate_playlist(lib, count, shuffle, input_query=""):
@@ -122,6 +142,8 @@ def generate_playlist(lib, count, shuffle, input_query=""):
     sorted_tracks = sorted(items, key=lambda track: -sum(track.scores.values()))
     log.debug(u"Running post rules")
     post_rule_limit_artists(sorted_tracks)
+    sorted_tracks = sorted(items, key=lambda track: -sum(track.scores.values()))
+    post_rule_limit_low_rating(sorted_tracks)
     sorted_tracks = sorted(items, key=lambda track: -sum(track.scores.values()))
 
     trimmed_tracks = sorted_tracks[0:count]
